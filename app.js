33c676e82138005d802977f580fca1a2f4656727
Samuel Hossain <samuel.hossain@outlook.com>
2024-09-23 12:31:50 -0400

    My Javascript Variables Exercise

--- app.js
+++ app.js
@@ -4,7 +4,48 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
 
 // Exercise 2
+firstName = "Mark";
+lastName = "Burnell";
+age = 30;
+
+//result for exercise 2
+console.log(`${firstName}${lastName} is {age} years old.`);
+
+
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//result for exercise 3
+console.log(`${language} was released on ${createdYear}`);
+
+
+// Exercise 4
+let product = "T-Shirt";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//result for exercise 4
+console.log(`${product} in ${selectedSize} is ${price} with ${stock} in stock.`);
+
+
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+//result for exercise 5
+console.log(`${title} is by ${author}.The book is ${pageCount} pages long and has ${bookmark} bookmarks.`);
